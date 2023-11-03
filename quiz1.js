7caf3c6cd06f54817ce3ea400fdcf0334c11bc8d
Tri jayanti hairani sitio <149707023+TriJayantiHairaniSitio@users.noreply.github.com>
2023-11-03 08:33:25 +0700

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,14 @@
+const restaurant = {
+  name: "Cindelaras",
+  city: "Pematangsiantar",
+  favoriteDrink: "Macha",
+  favoriteFood: "Seafood",
+  isVegan: true,
+};
+
+const name = restaurant.name;
+const favoriteDrink = restaurant.favoriteDrink;
+
+console.log("Nama Restoran:", restaurant.name);
+console.log("Minuman Favorit:", restaurant.favoriteDrink);
+console.log("Makanan Favorit:", restaurant.favoriteFood);
